296b65abe9e2c6800a60dbb82fb778704a080acd
9uru <9uruprasad@gmail.com>
2020-05-05 01:46:05 -0500

    added dnn (#2)
    * combined files unit tests
    
    * removed testdata and coverage
    
    * bring back testdata
    
    * readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # cv_projects
 Scripts and functions for computer vision test projects
+
+## Face Detect with DNN
+The prototxt and caffe model files can be found in testdata folder to provide as arguments
+source: pyimagesearch
--- tests/unit/test_face_detect.py
+++ tests/unit/test_face_detect.py
@@ -3,12 +3,13 @@ unit test for face detect
 '''
 
 from unittest.mock import MagicMock as Mock, patch
+import numpy as np
 from src.scripts import face_detect
 
 
 @patch('cv2.cvtColor')
 @patch('cv2.resize')
-def test_detect_draw_rect(cvtcol, resize):
+def test_detect_draw_rect_cascade(resize, cvtcol):
     '''
     test for detect and draw
     '''
@@ -17,5 +18,26 @@ def test_detect_draw_rect(cvtcol, resize):
     gray = Mock()
     cvtcol.return_value = gray
     resize.return_value = gray
-    frame = face_detect.detect_draw_rect(face_cascade, frame)
+    frame = face_detect.detect_draw_rect_cascade(face_cascade, frame)
     face_cascade.detectMultiScale.assert_called_with(gray, 1.1, 4)
+
+
+
+@patch('cv2.resize')
+@patch('cv2.dnn.blobFromImage')
+def test_detect_draw_rect_dnn(bbi, resize):
+    '''
+    test for detect and draw type dnn
+    '''
+    bbi = Mock()
+    resize = Mock()
+    frame = Mock()
+    frame.shape = (100, 100, 3)
+    blob = Mock()
+    net = Mock()
+    net.forward.return_value = np.zeros((1, 1))
+    confidence = 0.5
+    resize.return_value = blob
+    bbi.return_value = blob
+    frame = face_detect.detect_draw_rect_dnn(net, frame, confidence)
+    net.forward.assert_called()
